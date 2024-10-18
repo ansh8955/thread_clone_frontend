1324f39bb5142d5353b59b7eb499b2c4d91c43f5
Aniket jauhri <aniketjauhri2003@gmail.com>
2024-10-18 12:34:10 +0530

    done

---
+++ src/context/SocketContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
+import io from "socket.io-client";
+import userAtom from "../atoms/userAtom";
+
+const SocketContext = createContext();
+
+export const useSocket = () => {
+  return useContext(SocketContext);
+};
+
+export const SocketContextProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const user = useRecoilValue(userAtom);
+
+  useEffect(() => {
+    if (!user?._id) return; // Only connect if userId exists
+
+    // Replace "/" with your backend URL if backend is hosted separately
+    const socket = io("https://thread-clone-new-mxcp.onrender.com", {
+      query: {
+        userId: user._id, // Only connect when user._id exists
+      },
+    });
+
+    setSocket(socket);
+
+    socket.on("getOnlineUsers", (users) => {
+      setOnlineUsers(users);
+    });
+
+    return () => socket && socket.close(); // Clean up on component unmount
+  }, [user?._id]);
+
+  return (
+    <SocketContext.Provider value={{ socket, onlineUsers }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
